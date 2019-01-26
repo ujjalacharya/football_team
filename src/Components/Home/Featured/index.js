6b492996fadaae7f3d478fd258c981e3cb556e41
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-01-26 12:08:59 +0545

    add featured text

--- src/Components/Home/Featured/index.js
+++ src/Components/Home/Featured/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Stripes from './Stripes';
+import Text from './Text';
 
 function Featured() {
   return (
     <div className="featured_wrapper">
       <Stripes />
+      <Text />
     </div>
   )
 }
