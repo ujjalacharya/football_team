9b127bd30210c102642b8b03ecbeb373d63b2392
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-01-30 23:49:09 +0545

    add firebase to app

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import app from 'firebase/app';
+import 'firebase/app';
+import 'firebase/database';
+
+const config = {
+  apiKey: "AIzaSyDQ9UqABSO5LbU7QhtMSZAKRrVVae2Ea2c",
+  authDomain: "m-city-d4c15.firebaseapp.com",
+  databaseURL: "https://m-city-d4c15.firebaseio.com",
+  projectId: "m-city-d4c15",
+  storageBucket: "m-city-d4c15.appspot.com",
+  messagingSenderId: "648381341366"
+};
+
+
+app.initializeApp(config);
+
+const firebaseDb = app.database();
+
+firebaseDb.ref("matches").once("value")
+  .then(snapshot => console.log(snapshot.val()))
