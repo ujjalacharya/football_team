4f3a1b1681e73c57c07ebf37bcda53cbf12bb8a6
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-02-02 23:54:58 +0545

    add public route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./Resources/css/app.css";
 import Layout from "./HOC/Layout";
-import {Switch, Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
 import Home from "./Components/Home";
 import SignIn from "./Components/signin";
 import Dashboard from "./Components/admin/Dashboard";
 import PrivateRoute from "./Components/auth_routes/PrivateRoute";
+import PublicRoute from "./Components/auth_routes/PublicRoute";
 
 class App extends Component {
   render(props) {
@@ -13,8 +14,8 @@ class App extends Component {
       <Layout>
         <Switch>
           <PrivateRoute {...this.props} exact component={Dashboard} path="/dashboard"/>
-          <Route exact component={SignIn} path="/signin"/>
-          <Route exact component={Home} path="/"/>
+          <PublicRoute {...this.props} restricted={true} exact component={SignIn} path="/signin" />
+          <PublicRoute {...this.props} exact component={Home} path="/" />
         </Switch>
       </Layout>
     </div>
